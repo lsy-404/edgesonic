53320a61ef31a52336caf58c89ed2d2fdb12ef49
Wuyilingwei <53390868+wuyilingwei@users.noreply.github.com>
2026-08-17 19:01:03 +0800

    feat(installer): vendor WinUIonWeb Fluent Design components
    Add a trimmed WinButton/WinCheckBox/WinInfoBar component set (plus
    their internal dependencies) from the GPL-3.0-licensed WinUIonWeb
    project, with upstream license and attribution notice.

---
+++ installer/src/vendor/winui/index.ts
@@ -0,0 +1,3 @@
+export { default as WinButton } from "./components/WinButton.vue";
+export { default as WinCheckBox } from "./components/WinCheckBox.vue";
+export { default as WinInfoBar } from "./components/WinInfoBar.vue";
